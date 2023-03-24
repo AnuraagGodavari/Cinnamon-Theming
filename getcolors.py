62d3d91d614d8e3ef5865c00357ca9f99aa01a24
AnuraagGodavari <anuraag.godavari@gmail.com>
2023-03-24 17:10:15 -0400

    added gitignore, palettes can now convert all hex to rgb

--- getcolors.py
+++ getcolors.py
@@ -1,25 +1,32 @@
 #!/usr/bin/python3
 
 import re, sys, pprint, json
-
-def rgb_to_hex(color):
-	pass
-
-def hex_to_rgb(color):
-	pass
+from convertcolors import *
 
 def getcolors(infile, outfile = None, mode = None):
 	
 	css = ""
 
+	convert_func = None
+	if mode == "hex":
+		convert_func = rgb_to_hex
+	elif mode in ("rgb", "rgba"):
+		convert_func = hex_to_rgb
+
 	with open(infile) as f:
 		css = f.read()
 
-	palette = {color: "" for color in set([''.join(patterns) for patterns in re.findall("(rgba?\(.*\))|(#[1234567890abcdeABCDE]{6})", css)])}
+	patterns = set([''.join(matches) for matches in re.findall("(rgba?\(.*\))|(#[1234567890abcdefABCDEF]{6})", css)])
+	
+	if (convert_func): patterns = [convert_func(color) for color in patterns]
+
+	palette = {color: color for color in patterns}
 
 	if outfile:
 		with open(outfile, 'w') as f:
 			json.dump(palette, f, indent = 4)
+	
+	return palette
 
 	#pprint.pprint(list(palette))
 
@@ -28,10 +35,15 @@ def main():
 	infile = sys.argv[1]
 
 	outfile = None
+	mode = None
+
 	if (len(sys.argv) > 2):
 		outfile = sys.argv[2]
 
-	getcolors(infile, outfile)
+	if (len(sys.argv) > 3):
+		mode = sys.argv[3]
+
+	getcolors(infile, outfile, mode)
 
 if __name__ == '__main__':
 	if (len(sys.argv) <= 1):
